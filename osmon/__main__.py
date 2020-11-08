db2b8c887674594a03547724b5048941f4112626
prybiec <piotrrybiec28@gmail.com>
2020-11-08 11:14:35 +0100

    basic set

--- osmon/__main__.py
+++ osmon/__main__.py
@@ -3,12 +3,11 @@ import argparse
 import psutil
 
 
-
 parser = argparse.ArgumentParser(description='Get some stats.')
-parser.add_argument('--cpu')
-parser.add_argument('--gpu')
-parser.add_argument('--ram')
-parser.add_argument('--all')
+parser.add_argument('--cpu','-c', action='store_true')
+parser.add_argument('--gpu','-g', action='store_true')
+parser.add_argument('--ram','-r', action='store_true')
+parser.add_argument('--all','-a', action='store_true')
 args = parser.parse_args() 
 
 def cpu():
@@ -20,11 +19,20 @@ def ram():
 def gpu():
     pass
 
+def run_all()
+    pass
 
 def main():
-    pass    
+    if args.cpu or args.c:
+        cpu()
+    elif args.gpu or args.g:
+        gpu()
+    elif args.ram or args.r:
+        ram()
+    elif args.all or args.a:
+        run_all()
+
 
 
 if __name__ == '__main__':
-
     main()
